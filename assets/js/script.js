60707accd7c15421a63c3bace49eb337f528bd09
jAmWhisk <whisk3yjam@gmail.com>
2022-01-15 01:26:42 -0500

    basic html content set up and styled the buttons and add a timer

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var timerEl = document.getElementById('countdown');
+
+function countdown() {
+    var timeLeft = 75;
+  
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerEl.textContent = 'Time remaining: ' + timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textcontent = 'Time remaining: 0';
+
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+
+countdown();
