9a8bef9945a63177ea29521bc33c0a76e40dd6fe
jAmWhisk <whisk3yjam@gmail.com>
2022-03-22 21:02:56 -0400

    fix play again loop bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,12 @@ var gameoverEl = document.getElementById('finish');
 var answerEl = document.querySelectorAll('.answer');
 var submitEl = document.getElementById('save-initials');
 var playAgainEl = document.getElementById('play-again');
+var finalScoreEl = document.getElementById('final-score');
 var initialEl = document.getElementById('initials');
 var score = 0;
-
+var viewEl = document.getElementById('view');
+var quizContent = document.getElementById('contents');
+var scorePageEl = document.getElementById('score-page');
 
 var questions = [
     {
@@ -41,17 +44,14 @@ var questions = [
     }
 ];
 
-/*function playAgain() {
-    titlePageEl.removeAttribute('class', 'hide');
-    titlePageEl.setAttribute('class', 'title-page');
-    gameoverEl.setAttribute('class', 'hide');
-    startButtonEl.addEventListener('click', startQuiz);
-}*/
+function playAgain() {
+    window.location.reload();
+}
 
 function startQuiz() {
     // hides the title page when the quiz starts and display the questions
     titlePageEl.setAttribute('class', 'hide');
-    questionSectionEl.removeAttribute('class', 'hide');
+    questionSectionEl.classList.remove('hide');
     countdown();
     displayQuestion();
     console.log("quiz start");
@@ -59,9 +59,9 @@ function startQuiz() {
 
 function displayQuestion() {
     // get current question by current index
-    questionTextEl.textContent = questions[questionNumber].question;
     // loop through the array of answers and display
     for (var i = 0; i < answerEl.length; i++) {
+        questionTextEl.textContent = questions[questionNumber].question;
         answerEl[i].textContent = questions[questionNumber].answers[i];
         // handles the logic for when a question is answered i.e. a answer button is clicked 
         answerEl[i].addEventListener("click", answeredQuestion);
@@ -85,48 +85,70 @@ function answeredQuestion() {
     } else {
         displayQuestion();
     }
-
 }
 
+// display the gameover screen wtih high score text field and play again button
 function gameover() {
-    // display the gameover screen wtih high score text field and play again button
     questionSectionEl.setAttribute('class', 'hide');
-    gameoverEl.removeAttribute('class', 'hide');
-    submitEl.addEventListener("click", highscore);
-    //playAgainEl.addEventListener("click", playAgain);
-
+    gameoverEl.classList.remove('hide');
+    submitEl.addEventListener('click', highscore);
+    finalScoreEl.textContent = 'Your final score is ' + score + '.'
 }
 
+// save submitted data in local storage
 function highscore(event) {
     event.preventDefault();
     var initial = initialEl.value;
-    console.log(initial);
-    localStorage.setItem(initial, JSON.stringify(score));
+    if(localStorage.getItem('initials') == null) {
+        localStorage.setItem('initials', '[]');
+    }
+    var allData = JSON.parse(localStorage.getItem('initials'));
 
+    var initialScores = initial + ' - ' + score;
+    allData.push(initialScores);
+    console.log(allData)
+    localStorage.setItem('initials', JSON.stringify(allData));
+
+}
+
+function viewHighscore() {
+    quizContent.setAttribute('class', 'hide');
+    scorePageEl.classList.remove('hide');
+
+    var initial = JSON.parse(localStorage.getItem('initials'));
+    for(let i = 0; i < initial.length; i++) {
+        var header = document.createElement('h3');
+        header.textContent = initial[i];
+        header.setAttribute('class', 'past-score');
+        scorePageEl.appendChild(header);
+    }
+
+    var restartBtn = document.createElement('button');
+    restartBtn.textContent = 'Restart Quiz';
+    scorePageEl.appendChild(restartBtn);
+    restartBtn.setAttribute('class', 'past-score');
+    restartBtn.onclick = playAgain;
 }
 
 //countdown timer
 function countdown() {
-
     var timeInterval = setInterval(function () {
         if (timeLeft >= 1 && questionNumber > questions.length - 1) {
             clearInterval(timeInterval);
             gameover();
-            console.log("interval clear");
-            
+            console.log("interval clear"); 
         } else if (timeLeft > 0) {
             timerEl.textContent = 'Time remaining: ' + timeLeft;
             timeLeft--;
-            
         } else if (timeLeft === 0) {
             gameover();
             timerEl.textContent = 'Time remaining: ' + timeLeft;
-
         } else {
             clearInterval(timeInterval);
-
         }
     }, 1000);
 }
 
 startButtonEl.onclick = startQuiz;
+playAgainEl.onclick = playAgain;
+viewEl.onclick = viewHighscore;
